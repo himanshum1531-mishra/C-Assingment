66b7b3d1e9483c5c710194f29ddc1327f6453638
Himanshu mishra <himanshu.mishra@rungta.org>
2026-01-03 10:35:44 +0530

    Add files via upload

---
+++ assing_5_3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i, count = 1;
+
+    printf("Enter a sentence: ");
+    gets(str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ')
+            count++;
+    }
+
+    printf("Number of words = %d\n", count);
+
+    return 0;
+}
